6fc2bb19aaaa094c9b37db6f153003e4c685fbab
Kuchuk Andrey <kreezag@gmail.com>
2019-09-25 21:17:03 +0300

    add api service

---
+++ app/src/services/apiService.js
@@ -0,0 +1,47 @@
+const apiPeople = 'https://swapi.co/api/people/';
+
+const makeGetFetch = (url, { searchParams = {} } = {}) => {
+  const requestUrl = new URL(url);
+
+  const isEmptyObj = searchParams => Object.keys(searchParams).length;
+
+  if (isEmptyObj(searchParams)) {
+    Object.entries(searchParams).forEach((key, value) => {
+      requestUrl.searchParams.set(key, value);
+    });
+  }
+
+  const request = new Request(requestUrl.toString(), { method: 'GET' });
+
+  return fetch(request);
+};
+
+const getPeoples = (/*count, offset*/) => {
+  let resultPeoples = [];
+
+  const mkRequest = url =>
+    makeGetFetch(url)
+      .then(res => res.json())
+      .then(data => {
+        if (data.results) {
+          resultPeoples = [...resultPeoples, ...data.results];
+        }
+
+        if (data.next) {
+          return mkRequest(data.next);
+        }
+      });
+
+  return mkRequest(apiPeople).then(
+    () => ({
+      data: resultPeoples,
+      error: null,
+    }),
+    () => error => ({
+      data: resultPeoples,
+      error,
+    }),
+  );
+};
+
+export { getPeoples };
