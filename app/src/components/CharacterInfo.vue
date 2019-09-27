9a0de8ef36fb3f4814abfd608d24868c937be1d4
Kuchuk Andrey <kreezag@gmail.com>
2019-09-27 20:50:56 +0300

    add empty character-info component

---
+++ app/src/components/CharacterInfo.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Hello i am people {{ $route.params.id }}!</h2>
+</template>
--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <CharacterInfo></CharacterInfo>
   </div>
 </template>
+
+
+<script lang="ts">
+  import Vue from 'vue';
+  import CharacterInfo from '../components/CharacterInfo.vue';
+
+  export default Vue.extend({
+    components: {
+      CharacterInfo,
+    },
+  });
+</script>
