c2831adc9f496c64477375ce874866c85095434f
Kuchuk Andrey <kreezag@gmail.com>
2019-09-28 20:08:34 +0300

    create test slot-component

---
+++ app/src/components/TestSlot.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <slot>DEFAULT TEXT</slot>
+  </div>
+</template>
--- app/src/views/Test.vue
+++ app/src/views/Test.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="test">
     <h1>This is an test page</h1>
+    <TestSlot>Parent text</TestSlot>
+    <TestSlot />
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import TestSlot from '@/components/TestSlot.vue';
+
+export default Vue.extend({
+  components: {
+    TestSlot,
+  },
+});
+</script>
