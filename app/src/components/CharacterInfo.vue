489feed958a2f56c08015cae6d35a1e091dd278c
Kuchuk Andrey <kreezag@gmail.com>
2019-09-28 18:24:43 +0300

    add get-people-data to character-info

--- app/src/components/CharacterInfo.vue
+++ app/src/components/CharacterInfo.vue
@@ -1,3 +1,29 @@
 <template>
   <h2>Hello i am people {{ $route.params.id }}!</h2>
 </template>
+
+
+<script lang="ts">
+  import { getPeopleData } from '@/services/apiService';
+
+
+  export default {
+    data() {
+      return {
+        data: {},
+        loading: false
+      };
+    },
+    created: function() {
+      const self: any = this;
+
+      self.loading = true;
+
+      getPeopleData(self.$route.params.id).then(data => {
+        self.data = data;
+        self.loading = false;
+      });
+
+    },
+  };
+</script>
